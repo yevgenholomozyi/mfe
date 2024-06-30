2f0e36ce41f984d82a53d51ed02c08c5845bb505
yevhenHolomozyi <93094833+yevhenHolomozyi@users.noreply.github.com>
2024-06-30 20:06:34 +0200

    resolve class colissions

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <MarketingApp />
-      </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <Header />
+            <MarketingApp />
+          </StylesProvider>
+        </BrowserRouter>
     </>
   );
 };
